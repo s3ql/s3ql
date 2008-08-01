af78c0cfdcc35246ef9a3104fc1616d244f09b9c
Nikratio <none@none>
2008-08-01 10:24:27 +0000

    * Added file s3.py, forgotten in revision 11.
    --HG--
    extra : convert_revision : svn%3A0275539e-8e52-0410-9d9c-41c7838199bd/trunk%4017

---
+++ s3ql/s3.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+#
+#    Copyright (C) 2008  Nikolaus Rath <Nikolaus@rath.org>
+#
+#    This program can be distributed under the terms of the GNU LGPL.
+#
+import hashlib
+from time import time, sleep
+from boto.s3.connection import S3Connection
+import boto.exception
+
+class Connection(object):
+    """Represents a connection to Amazon S3
+
+    Currently, this just dispatches everything to boto.
+    """
+
+    def __init__(self, awskey, awspass, encrypt=None):
+        self.awskey = awskey
+        self.awspass = awspass
+        self.encrypt = encrypt
+
+        self.boto = S3Connection(awskey, awspass)
+
+    def delete_bucket(self, name, recursive=False):
+        """Deletes a bucket from S3.
+
+        Fails if there are objects in the bucket and `recursive` is
+        `False`.
+        """
+        if recursive:
+            bucket = self.boto.get_bucket(bucketname)
+            for s3key in bucket.list():
+                bucket.delete_key(s3key)
+
+        boto.delete_bucket(bucketname)
+
+    def create_bucket(self, name):
+        """Creates an S3 bucket
+        """
+        boto.create_bucket(bucketname)
+
+
+    def get_bucket(self, name):
+        """Returns a bucket instance for the bucket `name`
+
+        If the bucket doesn't exist, it is created first.
+        """
+        if not self.bucket_exists(name):
+            self.create_bucket(name)
+            # S3 needs some time before we can fetch the bucket
+            sleep(5)
+
+        return Bucket(self, name)
+
+    def bucket_exists(self, name):
+        """Checks if the bucket `name` exists.
+        """
+        try:
+            self.boto.get_bucket(name)
+        except boto.exception.S3ResponseError, e:
+            if e.status == 404:
+                return False
+            else:
+                raise
+        else:
+            return True
+
+
+
+class Bucket(object):
+    """Represents a bucket stored in Amazon S3.
+
+    This class should not be instantiated directly, but using
+    `Connection.get_bucket()`.
+
+    The class behaves more or less like a dict. It raises the
+    same exceptions, can be iterated over and indexed into.
+
+    """
+
+    def __init__(self, conn, name):
+        self.name = name
+        self.conn = conn
+        self.bucket = conn.boto.get_bucket(name)
+
+    def __str__(self):
+        return "<bucket: %s>" % self.name
+
+    def __getitem__(self, key):
+        return self.fetch(key)[0]
+
+    def __setitem__(self, key, value):
+        self.store(key, value)
+
+    def __delitem__(self, key):
+        self.delete_key(key)
+
+    def __iter__(self):
+        for (key,metadata) in self.list_keys():
+            yield key
+
+    def lookup_key(self, key):
+        """Return metadata for given key.
+
+        If the key does not exist, `None` is returned. Otherwise a
+        `Metadata` instance is returned.
+        """
+        bkey = self.bucket.get_key(key)
+
+        if bkey:
+            return self.boto_key_to_metadata(bkey)
+        else:
+            return None
+
+    def delete_key(self, key):
+        """Deletes the specified key
+
+        ``bucket.delete_key(key)`` can also be written as ``del bucket[key]``.
+
+        """
+        self.bucket.delete_key(key)
+
+    def list_keys(self):
+        """List keys in bucket
+
+        Returns an iterator over all keys in the bucket. The iterator
+        generates tuples of the form (key, metadata), where metadata is
+        of the form returned by `lookup_key`.
+
+        This function is also used if the bucket itself is used in an
+        iterative context: ``for key in bucket`` is the same as ``for
+        (key,metadata) in bucket.list_keys()`` (except that the former
+        doesn't define the `metadata` variable).
+        """
+
+        return self.bucket.list_keys()
+
+
+    def fetch(self, key):
+        """Return data stored under `key`.
+
+        Returns a tuple with the data and metadata. If only the data
+        itself is required, ``bucket[key]`` is a more concise notation
+        for ``bucket.fetch(key)[0]``.
+        """
+
+        bkey = self.bucket.new_key(key)
+        val = bkey.get_contents_as_string()
+        metadata = self.boto_key_to_metadata(bkey)
+
+        return (val, metadata)
+
+    def store(self, key, val):
+        """Store data under `key`.
+
+        Returns the newly associated metadata as returned by
+        `lookup_key`.
+
+        If the metadata is not required, one can simply assign to the
+        subscripted bucket instead of using this function:
+        ``bucket[key] = val`` is equivalent to ``bucket.store(key,
+        val)``.
+        """
+
+        bkey = self.bucket.new_key(key)
+        bkey.set_contents_from_string(val)
+
+        return self.boto_key_to_metadata(bkey)
+
+    def fetch_to_file(self, key, file):
+        """Fetches data stored under `key` and writes them to `file`.
+
+        `file` has to be a file name. Returns the metadata in the
+        format used by `lookup_key`.
+        """
+        bkey = self.bucket.new_key(key)
+        bkey.get_contents_to_filename(file)
+
+        return self.boto_key_to_metadata(bkey)
+
+    def store_from_file(self, key, file):
+        """Reads `file` and stores the data under `key`
+
+        `file` has to be a file name. Returns the metadata in the
+        format used by `lookup_key`.
+        """
+        bkey = self.bucket.new_key(key)
+        bkey.set_contents_from_filename(file)
+
+        return self.boto_key_to_metadata(bkey)
+
+    def boto_key_to_metadata(self, bkey):
+        """Extracts metadata from boto key object.
+        """
+
+        meta = Metadata(bkey.metadata)
+        meta.etag = bkey.etag
+        meta.last_modified = datetime.strptime(bkey.last_modified, "%a, %d %b %Y %H:%M:%S %Z")
+        meta.size = bkey.size
+
+        return meta
+
+
+
+class LocalBucket(Bucket):
+    """Represents a bucket stored in Amazon S3.
+
+
+    This class doesn't actually connect but holds all data in
+    memory. It is meant only for testing purposes.
+    """
+
+    def __init__(self, name="local"):
+        self.keys = {}
+        self.name = name
+
+
+    def lookup_key(self, key):
+        if not self.keys.has_key(key):
+            return None
+        else:
+            return self.keys[key][0]
+
+    def list_keys(self, prefix=None, separator=None):
+        for key in self.keys:
+            yield self.keys[key]
+
+    def delete_key(self, key):
+        del self.keys[key]
+
+    def fetch(self, key):
+        return self.keys[key]
+
+    def store(self, key, val, metadata=None):
+        metadata = Metadata(metadata) if metadata else Metadata()
+        metadata.key = key
+        metadata.size = len(val)
+        metadata.last_modified = time()
+        metadata.etag =  hashlib.md5(val).hexdigest()
+        self.keys[key] = (val, metadata)
+        return metadata
+
+
+    def fetch_to_file(self, key, file):
+        (data, metadata) = self.fetch(key)
+        file = open(file, "wb")
+        file.write(data)
+        file.close()
+        return metadata
+
+    def store_from_file(self, key, file, metadata=None):
+        file = open(file, "rb")
+        value = file.read()
+        file.close()
+
+        return self.store(key, value, metadata)
+
+
+class Metadata(dict):
+    """Represents the metadata associated with an S3 object.
+
+    The "hardcoded" meta-information etag, size and last-modified are
+    accessible as instance attributes. For access to user defined
+    metadata, the instance has to be subscripted.
+
+    Note that the last-modified attribute is a datetime object.
+    """
+
+    pass
