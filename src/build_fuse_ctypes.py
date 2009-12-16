02129dbbe9308e365207aa364fb31d98fcf136f8
nikratio <none@none>
2009-12-16 03:31:40 +0000

    code generator is now ready for use.
    --HG--
    extra : convert_revision : svn%3A0275539e-8e52-0410-9d9c-41c7838199bd/trunk%40342

--- src/build_fuse_ctypes.py
+++ src/build_fuse_ctypes.py
@@ -1,31 +1,51 @@
 import os.path
+import re
 import sys
 import subprocess
 from pygccxml import parser
 from pygccxml import declarations
 from pyplusplus.module_builder import ctypes_module_builder_t, ctypes_decls_dependencies
 
-pkgconf = subprocess.Popen(['pkg-config', 'fuse', '--cflags'], stdout=subprocess.PIPE)
-(stdout, stderr) = pkgconf.communicate() # stderr will be None
-if pkgconf.returncode != 0:
+# Work out compilation flags
+proc = subprocess.Popen(['pkg-config', 'fuse', '--cflags'], stdout=subprocess.PIPE)
+cflags = proc.stdout.readline().rstrip()
+proc.stdout.close()
+if proc.wait() != 0:
     sys.stderr.write('Failed to execute pkg-config. Exit code: %d.\n' 
-                     % pkgconf.returncode)
+                     % proc.returncode)
+    sys.exit(1)
+print 'Using cflags: %s' % cflags    
+           
+# Work out library location
+proc = subprocess.Popen(['/sbin/ldconfig', '-p'], stdout=subprocess.PIPE)
+shared_library_path = None
+for line in proc.stdout:
+    res = re.match('^\\s*libfuse\\.so \\(libc6\\) => (.+)$', line)
+    if res is not None:
+        shared_library_path = res.group(1)
+        # Slurp rest of output
+        for _ in proc.stdout:
+            pass
+        
+proc.stdout.close()
+if proc.wait() != 0:
+    sys.stderr.write('Failed to execute /sbin/ldconfig. Exit code: %d.\n' 
+                     % proc.returncode)
+    sys.exit(1)
+
+if shared_library_path is None:
+    sys.stderr.write('Failed to locate fuse library libfuse.so.\n')
     sys.exit(1)
     
-# This confuses pylint
-#pylint: disable-msg=E1103
-cflags = stdout.lstrip().rstrip()
+print 'Found fuse library in %s' % shared_library_path
+    
        
 basedir = os.path.dirname(__file__)
 
 header_file = os.path.join(basedir, 'fuse_ctypes.h')
-symbols_file = '/usr/lib/libfuse.so'
-shared_library_file = 'libfuse.so'
-
 gccxml_cfg = parser.gccxml_configuration_t(cflags=cflags)
 
-mb = ctypes_module_builder_t( [header_file], symbols_file, gccxml_config=gccxml_cfg )
-#mb = ctypes_module_builder_t( [header_file], None, gccxml_config=gccxml_cfg )
+mb = ctypes_module_builder_t( [header_file], shared_library_path, gccxml_config=gccxml_cfg )
 
 # Work around bug in code generator
 has_varargs = lambda f: f.arguments \
@@ -49,6 +69,9 @@ for incl in include_symbols:
 for dep in ctypes_decls_dependencies.find_out_dependencies(include_symbols):
     dep.include()
             
-mb.build_code_creator(shared_library_file)
+mb.build_code_creator(shared_library_path)
 mb.write_module(os.path.join(basedir, 's3ql', 'fuse_ctypes.py'))
 
+os.unlink(os.path.join(basedir, 's3ql', 'exposed_decl.pypp.txt'))
+
+print 'Code generation complete.'          
