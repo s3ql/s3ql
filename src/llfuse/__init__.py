0b2ff831b920a7b3eb371a853d14dbf4c127621d
nikratio <none@none>
2009-12-31 20:29:22 +0000

    Added __all__
    --HG--
    extra : convert_revision : svn%3A0275539e-8e52-0410-9d9c-41c7838199bd/trunk%40414

--- src/llfuse/__init__.py
+++ src/llfuse/__init__.py
@@ -8,7 +8,10 @@ This program can be distributed under the terms of the GNU LGPL.
 
 from __future__ import unicode_literals, division, print_function
 
+__all__ = [ 'ctypes_api', 'interface', 'operations', 'example' ]
+
 # Wildcard imports desired
 #pylint: disable-msg=W0401
 from llfuse.operations import *
 from llfuse.interface import *
+
