b039a9ae9c0d788b101b5f277c0c04f18d0594ce
nikratio <none@none>
2010-01-11 03:42:42 +0000

    Added setup.py, setup.cfg and MANIFEST.in.
    --HG--
    extra : convert_revision : svn%3A0275539e-8e52-0410-9d9c-41c7838199bd/trunk%40475

--- setup.py
+++ setup.py
@@ -11,17 +11,17 @@ from __future__ import division, print_function
     
 from distutils.core import setup
 setup(name='s3ql',
-      version='1.0',
+      version='alpha3',
       description='a FUSE filesystem that stores data on Amazon S3',
       author='Nikolaus Rath',
       author_email='Nikolaus@rath.org',
       url='http://code.google.com/p/s3ql/',
       package_dir={'': 'src'},
-      packages=['s3ql'],
+      packages=['s3ql', 'llfuse'],
       provides=['s3ql'],
       scripts=[ 'bin/fsck.s3ql',
                 'bin/mkfs.s3ql',
                 'bin/mount.s3ql_local',
                 'bin/mount.s3ql' ],
-      requires=['apsw', 'boto'],
+      requires=['apsw', 'boto', 'pycryptopp' ],
      )
