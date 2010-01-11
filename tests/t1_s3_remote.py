7cf53e364b207d823ee1e2da821f357ed75b6b0b
nikratio <none@none>
2010-01-11 03:32:33 +0000

    list_keys: Take into account propagation delay.
    --HG--
    extra : convert_revision : svn%3A0275539e-8e52-0410-9d9c-41c7838199bd/trunk%40474

--- tests/t1_s3_remote.py
+++ tests/t1_s3_remote.py
@@ -76,6 +76,7 @@ class s3_tests_remote(unittest.TestCase):
         for i in range(12):
             self.bucket.store(keys[i], values[i])
 
+        sleep(s3ql.s3.LOCAL_PROP_DELAY)
         self.assertEquals(sorted(self.bucket.keys()), sorted(keys))
             
         for i in range(12):
