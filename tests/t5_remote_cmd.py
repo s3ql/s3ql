50a7a28a893d904de145ecac8f7c77397426ac7b
nikratio <none@none>
2010-01-21 19:26:50 +0000

    Run in separate thread and not separate process. Do not require S3 access anymore.
    --HG--
    extra : convert_revision : svn%3A0275539e-8e52-0410-9d9c-41c7838199bd/trunk%40635

--- tests/t5_remote_cmd.py
+++ tests/t5_remote_cmd.py
@@ -8,78 +8,107 @@ This program can be distributed under the terms of the GNU LGPL.
 
 from __future__ import division, print_function 
 
-import os
 from random import randrange
-from s3ql.common import waitfor
+from s3ql.common import waitfor, ExceptionStoringThread
 from s3ql import s3
-import tempfile
+import sys
+from cStringIO import StringIO
 import posixpath
 import unittest
-from _common import TestCase, get_aws_credentials
+import tempfile
+import time
 import subprocess
+from _common import TestCase
+import s3ql.cli.mkfs
+import s3ql.cli.mount
+import s3ql.cli.fsck
+import s3ql.cli.umount
+import shutil
     
-@unittest.skipUnless(get_aws_credentials(), 'remote tests disabled')
 class RemoteCmdTests(TestCase): 
-    
-    def setUp(self):
-        self.base = tempfile.mkdtemp()
 
-        # Find unused bucket
-        (awskey, awspass) = get_aws_credentials()
-        self.conn = s3.Connection(awskey, awspass)
-        
-        self.bucketname = self.random_name()
-        tries = 10
-        while self.conn.bucket_exists(self.bucketname) and tries > 10:
-            self.bucketname = self.random_name()
-            tries -= 1
-            
-        if tries == 0:
-            raise RuntimeError("Failed to find an unused bucket name.")
-
-
-    def tearDown(self):
-        self.conn.delete_bucket(self.bucketname, recursive=True)   
-            
     @staticmethod
     def random_name():
         return "s3ql" + str(randrange(10000, 99999, 1))
 
-    
-    def test_mount(self):
-        passphrase = 'blafasel'
+    def setUp(self):
+        self.base = tempfile.mkdtemp()
+        self.cache = tempfile.mkdtemp()
         
-        cmd =  os.path.join(os.path.dirname(__file__), "..", "bin", "mkfs.s3ql")
-        child = subprocess.Popen([cmd, "--blocksize", "1", '--quiet',
-                                           '--encrypt', self.bucketname], stdin=subprocess.PIPE)
-        child.communicate('%s\n%s\n' % (passphrase, passphrase))
-        self.assertEquals(child.returncode, 0)
-               
-        cmd = os.path.join(os.path.dirname(__file__), "..", "bin", "mount.s3ql")
-        child = subprocess.Popen([cmd, "--cachesize", "1", '--quiet', self.bucketname, self.base],
-                                 stdin=subprocess.PIPE)
-        child.stdin.write('%s\n' % passphrase)  
-        self.assertEqual(child.wait(), 0)     
-               
+        # Fake connection
+        s3.Connection = s3.LocalConnection
+        
+    def tearDown(self):
+        shutil.rmtree(self.base)
+        shutil.rmtree(self.cache)
+            
+    def test_mount(self):
+        
+        bucketname = 'test_bucket'
+        passphrase = 'foobar'
+        
+        # Create filesystem
+        sys.argv = ['mkfs.s3ql', '--awskey', 'foo', '-L', 'test fs', '--blocksize', '10',
+                    '--encrypt', '--quiet', '--cachedir', self.cache, bucketname ]
+        sys.argc = len(sys.argv)
+        sys.stdin = StringIO('bla\n%s\n%s\n' % (passphrase, passphrase))
+        try:
+            s3ql.cli.mkfs.main()
+        except SystemExit as exc:
+            if exc.code == 0:
+                pass
+            else:
+                self.fail("mkfs.s3ql failed with error code %d" % exc.code)
+        finally:
+            sys.stdin = sys.__stdin__
+            
+
+        # Mount filesystem
+        sys.argv = ['mount.s3ql', "--fg", "--quiet", '--awskey', 'foo',
+                    '--cachedir', self.cache, bucketname, self.base]
+        sys.argc = len(sys.argv)
+        sys.stdin = StringIO('foo\n%s\n' % passphrase)   
+        mount = ExceptionStoringThread(s3ql.cli.mount.main)
+        mount.start()
+
         # Wait for mountpoint to come up
         self.assertTrue(waitfor(10, posixpath.ismount, self.base))
 
         # Umount as soon as mountpoint is no longer in use
+        time.sleep(0.5)
         self.assertTrue(waitfor(5, lambda : 
-                                subprocess.call(['fuser', '-m', '-s', self.base]) == 1))
-        path = os.path.join(os.path.dirname(__file__), "..", "bin", "umount.s3ql")            
-        self.assertEquals(subprocess.call([path, '--quiet', self.base]), 0)
+                                    subprocess.call(['fuser', '-m', '-s', self.base]) == 1))
+        sys.argv = ['umount.s3ql', "--quiet", self.base]
+        sys.argc = len(sys.argv)
+        s3ql.cli.umount.DONTWAIT = True
+        try:
+            s3ql.cli.umount.main()
+        except SystemExit as exc:
+            if exc.code == 0:
+                pass
+            else:
+                self.fail("Umount failed with error code %d" % exc.code)
         
         # Now wait for server process
-        self.assertEquals(child.wait(), 0)
-        self.assertFalse(posixpath.ismount(self.base))
-            
-        os.rmdir(self.base)
+        exc = mount.join_get_exc()
+        self.assertTrue(isinstance(exc, SystemExit))
+        self.assertEqual(exc.code, 0)
         
-        cmd = os.path.join(os.path.dirname(__file__), "..", "bin", "fsck.s3ql")
-        child = subprocess.Popen([cmd, "--quiet", self.bucketname], stdin=subprocess.PIPE)
-        child.stdin.write('%s\n' % passphrase)  
-        self.assertEqual(child.wait(), 0)     
+        self.assertFalse(posixpath.ismount(self.base))
+        
+        # Now run an fsck
+        sys.argv = ['fsck.s3ql', '--awskey', 'foo', '--quiet', '--cachedir', self.cache, bucketname ]
+        sys.argc = len(sys.argv)
+        sys.stdin = StringIO('foo\n%s\n' % passphrase)
+        try:
+            s3ql.cli.fsck.main()
+        except SystemExit as exc:
+            if exc.code == 0:
+                pass
+            else:
+                self.fail("fsck.s3ql failed with error code %d" % exc.code)
+        finally:
+            sys.stdin = sys.__stdin__                
 
 
 # Somehow important according to pyunit documentation
