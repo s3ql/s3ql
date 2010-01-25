0fde645873b9d17d013b0ebfd50de48942f76757
nikratio <none@none>
2010-01-25 22:15:45 +0000

    Set version to beta1.
    --HG--
    extra : convert_revision : svn%3A0275539e-8e52-0410-9d9c-41c7838199bd/trunk%40706

--- setup.py
+++ setup.py
@@ -257,7 +257,7 @@ class run_tests(Command):
 
 
 setup(name='s3ql',
-      version='1.0',
+      version='beta1',
       description='a FUSE filesystem for storing data in Amazon S3',
       author='Nikolaus Rath',
       author_email='Nikolaus@rath.org',
