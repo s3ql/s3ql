40e5592964c1310c282f54d48d255c29e17674ec
nikratio <none@none>
2010-01-30 19:50:15 +0000

    Make sure that logging is initialized. Whitespace fixups.
    --HG--
    extra : convert_revision : svn%3A0275539e-8e52-0410-9d9c-41c7838199bd/trunk%40731

--- tests/_common.py
+++ tests/_common.py
@@ -6,30 +6,33 @@ Copyright (C) 2008-2009 Nikolaus Rath <Nikolaus@rath.org>
 This program can be distributed under the terms of the GNU LGPL.
 '''
 
-from __future__ import division, print_function 
+from __future__ import division, print_function
 
 import unittest
 import os
 import stat
 import sys
+import logging
+
+from s3ql.common import init_logging
 
 __all__ = [ 'TestCase', 'get_aws_credentials' ]
 
 aws_credentials_available = None
 aws_credentials = None
-keyfile = os.path.join(os.environ["HOME"], ".awssecret")  
+keyfile = os.path.join(os.environ["HOME"], ".awssecret")
 
 class TestCase(unittest.TestCase):
-    
+
     def __init__(self, *a, **kw):
         super(TestCase, self).__init__(*a, **kw)
-        
+
     def run(self, result=None):
         if result is None:
             result = self.defaultTestResult()
-        
+
         super(TestCase, self).run(result)
-        
+
         # Abort if any test failed
         if result.errors or result.failures:
             result.stop()
@@ -40,36 +43,39 @@ def get_aws_credentials():
     
     Otherwise return None and, on first invocation, print warning message.
     '''
-    
+
     global aws_credentials
     global aws_credentials_available
-    
+
     # Check only if not done so already
     if aws_credentials_available:
         return aws_credentials
-    
+
     elif aws_credentials_available is False: # distinguish between None and False
         return
-    
+
     if os.path.isfile(keyfile):
         mode = os.stat(keyfile).st_mode
         kfile = open(keyfile, "r")
         key = kfile.readline().rstrip()
-    
+
         if mode & (stat.S_IRGRP | stat.S_IROTH):
             sys.stderr.write("~/.awssecret has insecure permissions, skipping remote tests.\n")
             aws_credentials_available = False
             return
-        else:    
+        else:
             pw = kfile.readline().rstrip()
             aws_credentials = (key, pw)
-            aws_credentials_available = True        
+            aws_credentials_available = True
             print ('Will use credentials from ~/.awssecret to access AWS.')
             return aws_credentials
-        kfile.close()  
+        kfile.close()
     else:
         print('~/.awssecret does not exist. Will skip tests requiring valid AWS credentials.')
         aws_credentials_available = False
         return
-        
-    
+
+# Set some default logging settings (unless logging
+# is already configured)
+if not logging.getLogger().handlers:
+    init_logging(logfile=None, stdout_level=logging.WARN)
