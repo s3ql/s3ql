ff0c5ed17a0ec2606e4a3ce30932316412fd4c96
nikratio <none@none>
2010-04-04 22:20:23 -0400

    Really removed  boto warning handler here.
    --HG--
    extra : rebase_source : f44bc25ae017a322eed6816588c1b1f32a6608ff

--- src/s3ql/__init__.py
+++ src/s3ql/__init__.py
@@ -8,9 +8,5 @@ This program can be distributed under the terms of the GNU LGPL.
 
 from __future__ import division, print_function
 
-# Python boto uses several deprecated modules, deactivate warnings for them
-import warnings
-warnings.filterwarnings("ignore", "", DeprecationWarning, "boto")
-
 __all__ = [ "common", 'daemonize', 'database', 'fs', 'fsck', 'libc_api',
             "mkfs", 'multi_lock', 'ordered_dict', 's3cache' ]
