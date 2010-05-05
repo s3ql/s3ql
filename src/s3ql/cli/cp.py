5d926881f98a9bc539a22a3a19c5b34eba398c5c
Nikolaus Rath <Nikolaus@rath.org>
2010-05-05 14:47:11 -0400

    Updated docstrings.

--- src/s3ql/cli/cp.py
+++ src/s3ql/cli/cp.py
@@ -1,5 +1,5 @@
 '''
-umount.py - this file is part of S3QL (http://s3ql.googlecode.com)
+cp.py - this file is part of S3QL (http://s3ql.googlecode.com)
 
 Copyright (C) 2008-2009 Nikolaus Rath <Nikolaus@rath.org>
 
