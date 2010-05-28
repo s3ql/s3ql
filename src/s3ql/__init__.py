75cc6aa590078dc9ce0edf3d653e28a23699e7d4
Nikolaus Rath <Nikolaus@rath.org>
2010-05-28 19:48:40 -0400

    Documented fixed tests, bump version to 0.15

--- src/s3ql/__init__.py
+++ src/s3ql/__init__.py
@@ -11,5 +11,5 @@ from __future__ import division, print_function
 __all__ = [ 'block_cache', "common", 'daemonize', 'database', 'fs', 'fsck', 'libc_api',
             'libc', "mkfs", 'multi_lock', 'ordered_dict', 'VERSION', 'CURRENT_FS_REV' ]
 
-VERSION = '0.14'
+VERSION = '0.15'
 CURRENT_FS_REV = 7
