e1eb3086171afacf6bbeae2c05659bda29506b24
Nikolaus Rath <Nikolaus@rath.org>
2010-06-13 19:01:08 -0400

    Define __builtin_va_arg_pack as some random type.

--- src/fuse_ctypes.h
+++ src/fuse_ctypes.h
@@ -1,7 +1,9 @@
 /* Necessary to prevent gccxml from complaining about
- * an undefined type */
+ * an undefined type. Settings this to a random type should
+ * be safe as long as the FUSE API does not start using
+ * variable numbers of arguments. */
 #define __builtin_va_arg_pack_len int
-
+#define __builtin_va_arg_pack int
 
 #define FUSE_USE_VERSION 28
 #include <fuse_lowlevel.h>
