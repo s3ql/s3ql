44d21cf913a8ab2bd3ec72a9f4522560b08ad0cd
Nikolaus Rath <Nikolaus@rath.org>
2010-09-20 15:53:09 -0400

    Updated semantics to use date ranges. Documentation update still required.

--- contrib/expire_backups.py
+++ contrib/expire_backups.py
@@ -41,9 +41,12 @@ def parse_args(args):
         This program deletes backups that are no longer needed as defined by the
         specified backup strategy. It uses a sophisticated algorithm that
         ensures that there will always be at least one backup available in each
-        the specified  "generations". A generation is specified by giving its
-        relative age compared to the younger generation. Please refer to the
-        S3QL dokumentation for details.
+        of a number of age ranges. Age ranges are defined by giving the end
+        of each range relative to the end of the previous range. The
+        first range ends with the most recent backup. If there is no backup
+        available for a age range, the ends of all previous ranges are
+        shifted backwards in time until there is a backup available.
+        Please refer to the S3QL documentation for details.
         '''))
 
     parser.add_quiet()
@@ -52,20 +55,20 @@ def parse_args(args):
     
     def age_type(s):
         if s.endswith('h'):
-            return timedelta(seconds=int(s[:-1]) * 3600)
+            return -timedelta(seconds=int(s[:-1]) * 3600)
         elif s.endswith('d'):
-            return timedelta(days=int(s[:-1]))
+            return -timedelta(days=int(s[:-1]))
         else:
             raise argparse.ArgumentTypeError('%s is not a valid age.' % s)          
         
-    parser.add_argument('generations', nargs='+', help='backup ages to keep',
+    parser.add_argument('ages', nargs='+', help='Relative beginnings of age ranges to keep',
                         type=age_type, metavar='<age>')
 
     parser.add_argument("-n", action="store_true", default=False,
                         help="Dry run. Just show which backups would be deleted.")
 
     parser.add_argument("--use-s3qlrm", action="store_true",
-                      help="Use `s3qlrm` command to delete directories.")
+                      help="Use `s3qlrm` command to delete backups.")
         
     return parser.parse_args(args)
 
@@ -78,14 +81,11 @@ def main(args=None):
     setup_logging(options)
 
     # Relative generation ages
-    generations = options.generations
-    for (i, gen) in enumerate(options.generations):
-        if i == 0:
-            log.info('Generation %d starts %s after most recent backup',
-                     i+1, gen)
-        else:
-            log.info('Generation %d starts %s after first backup of generation %d',
-                     i+1, gen, i)
+    ages = options.ages
+    log.info('Age range 0 ends with most recent backup')
+    for (i, gen) in enumerate(ages):
+        log.info('Age range %d ends %s hours before end of range %d',
+                 i+1, -gen, i)
 
     # Determine available backups
     available_txt = sorted(x for x in os.listdir('.')
@@ -99,20 +99,21 @@ def main(args=None):
     available = list()
     i = 0
     for name in reversed(available_txt):
-        age = now - datetime.strptime(name, '%Y-%m-%d_%H:%M:%S')
+        age = datetime.strptime(name, '%Y-%m-%d_%H:%M:%S')
         available.append((name, age))
         i += 1
-        log.info('Backup %d is from %s, age: %s', i, name, age)
-
-    step = min(generations)
+        log.info('Backup %d is from %s, age: %s hours', i, name, age)
+ 
     if not available:
         return
+    step = -max(ages)
+    log.debug('Stepsize is %s hours', step)
 
-    if available[0][1] < step:
-        log.warn('NOTE: Your most recent backup is %s old, but according to your backup\n'
-                 'strategy, it should be at least %s old before creating a new backup.\n'
-                 'Are you sure that you specified the backup generations correctly?',
-                 available[0][1], step)
+    if now - available[0][1] < step:
+        log.warn('NOTE: Your most recent backup is %s hours old, but according to your backup\n'
+                 'strategy, it should be at least %s hours old before creating a new backup.\n'
+                 'Are you sure that you specified the age ranges correctly?',
+                 now - available[0][1], step)
 
     # Backups that need to be kept
     keep = dict()
@@ -123,64 +124,62 @@ def main(args=None):
     while True:
         log.debug('Considering situation on %s', now + simulated)
 
-        # Go through generations
-        cur_age = timedelta(0)
-        done = True
-        for (i, min_rel_age) in enumerate(generations):
-            if cur_age < simulated: # Future backup
-                cur_age = cur_age + min_rel_age
+        # Determine age ranges
+        ranges = list()
+        beginning = available[0][1] + simulated # beginning of current range
+        for (i, min_rel_age) in enumerate(ages):
+            beginning += min_rel_age
+            log.debug('Age range %d ends at %s', i+1, beginning)
+            ranges.append(beginning)
+            
+        for i in range(len(ranges)-1):
+            
+            # If that range is actually in the future, we don't need a backup
+            # for it
+            if ranges[i] > now:
                 continue
-            done = False
-            min_age = cur_age + min_rel_age
-            log.debug('Minimum age for generation %d is %s', i+1, min_age)
-            for (j, age) in enumerate((a[1] for a in available)):
-                if age >= min_age:
+            
+            for (j, age) in enumerate((a[1] for a in available[1:])):
+                if age < ranges[i]:
+                    if age <= ranges[i+1]:
+                        dt = age - ranges[i+1]
+                        log.debug('No backup for age range %d, moving subsequent '
+                                  'ranges backward by %s hours', i+1, -dt)
+                        for k in range(i+1, len(ranges)):
+                            ranges[k] += dt
+                            log.debug('Age range %d now ends at %s', k+1, ranges[k])
+                            
                     if j not in keep:
-                        if simulated:
-                            log.info('Keeping backup %d, required for generation %d in %s hours',
-                                     j+1, i + 1, simulated)
-                        else:
-                            log.info('Keeping backup %d for generation %d',
-                                     j+1, i + 1)
-                    cur_age = age
+                        log.info('Keeping backup %d, for age range %d%s',
+                                 j+1, i + 1, format_future(simulated))
+                    else:
+                        log.debug('Keeping backup %d, for age range %d%s',
+                                  j+1, i + 1, format_future(simulated))
                     keep[j] = True
                     break
             else:
                 if available:
                     keep[len(available) - 1] = True
                     if warn_missing:
-                        if simulated:
-                            log.warn('There will be no sufficiently old backup for generation %d in %s hours, '
-                                     'keeping backup %d instead.\n'
-                                     '(further warnings about missing backups will be suppressed)',
-                                     i + 1, simulated, len(available))
-                        else:
-                            log.warn('Found no sufficiently old backup for generation %d, '
-                                     'keeping backup %d instead.\n'
-                                     '(further warnings about missing backups will be suppressed)',
-                                     i + 1, len(available))
-
+                        log.warn('There will be no sufficiently old backup for age range %d%s '
+                                 'keeping backup %d instead.\n'
+                                 '(further warnings about missing backups will be suppressed)',
+                                 i + 1, format_future(simulated), len(available))
                         warn_missing = False
                 else:
                     if warn_missing:
-                        if simulated:
-                            log.warn('There will be no backup for generation %d in %s hours\n'
-                                     '(further warnings about missing backups will be suppressed)',
-                                     simulated, i + 1)
-                        else:
-                            log.warn('Found no backup for generation %d\n'
-                                     '(further warnings about missing backups will be suppressed)',
-                                     simulated, i + 1)
-
+                        log.warn('There will be no backup for age range %d%s\n'
+                                 '(further warnings about missing backups will be suppressed)',
+                                 format_future(simulated), i + 1)
                         warn_missing = False
                 break
 
-        # Update backup ages
-        simulated += step
-        available = [ (a[0], a[1] + step) for a in available ]
-
-        if done:
+        # If all ranges are in the future, we are done
+        if ranges[-1] > now:
             break
+        
+        simulated += step
+
 
     # Remove what's left
     for name in [ available[i][0] for i in range(len(available)) if i not in keep ]:
@@ -191,6 +190,11 @@ def main(args=None):
             else:
                 shutil.rmtree(name)
 
-
+def format_future(delta):
+    if delta:
+        return ' in %s hours' % delta
+    else:
+        return ''
+    
 if __name__ == '__main__':
     main(sys.argv[1:])
