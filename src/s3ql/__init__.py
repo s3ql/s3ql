bf1930cb0b4ddf16b2cccf9f1111882edb0d9ede
Nikolaus Rath <Nikolaus@rath.org>
2010-09-23 12:21:32 -0400

    Update __all__.

--- src/s3ql/__init__.py
+++ src/s3ql/__init__.py
@@ -8,9 +8,10 @@ This program can be distributed under the terms of the GNU LGPL.
 
 from __future__ import division, print_function
 
-__all__ = [ 'block_cache', "common", 'daemonize', 'database', 'fs', 'fsck', 'libc_api',
-            'libc', "mkfs", 'multi_lock', 'argparse', 'ordered_dict', 'VERSION', 
-            'CURRENT_FS_REV', 'thread_group', 'upload_manager' ]
+__all__ = [ 'backends', 'cli', 'argparse', 'block_cache', "common", 'daemonize', 
+            'database', 'fs', 'fsck', 'libc_api',
+            'libc', "mkfs", 'multi_lock', 'ordered_dict', 'thread_group',
+            'upload_manager', 'VERSION', 'CURRENT_FS_REV' ]
 
 VERSION = '0.24'
 CURRENT_FS_REV = 9
