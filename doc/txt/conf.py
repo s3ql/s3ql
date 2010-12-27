858ad913b4c9e25316c8435ff993945dc7c80022
Nikolaus Rath <Nikolaus@rath.org>
2010-12-27 17:09:50 -0500

    Be nitpicky about missing references.

--- doc/txt/conf.py
+++ doc/txt/conf.py
@@ -17,7 +17,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-#extensions = [ 'rst2pdf.pdfbuilder' ]
+#extensions = [ ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -30,6 +30,9 @@ source_encoding = 'utf-8'
 
 # The master toctree document.
 master_doc = 'index'
+
+# Warn about unresolved references
+nitpicky = True
 
 # General information about the project.
 project = u'S3QL'
@@ -189,83 +192,12 @@ latex_documents = [
 # If false, no module index is generated.
 #latex_use_modindex = True
 
-# -- Options for PDF output --------------------------------------------------
-pdf_documents = [ 
-  ('index', 'manual.pdf', u'S3QL Documentation',
-   u'Nikolaus Rath', 'manual'),
+
+# -- Options for manual page output --------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    ('mount', 'mount.s3ql', u'S3QL Documentation',
+     [u'Nikolaus Rath'], 1)
 ]
-
-# A comma-separated list of custom stylesheets. Example:
-pdf_stylesheets = ['sphinx','kerning','a4']
-
-# Create a compressed PDF
-# Use True/False or 1/0
-# Example: compressed=True
-pdf_compressed = True
-
-# A colon-separated list of folders to search for fonts. Example:
-# pdf_font_path = ['/usr/share/fonts', '/usr/share/texmf-dist/fonts/']
-
-# Language to be used for hyphenation support
-pdf_language = "en_US"
-
-# Mode for literal blocks wider than the frame. Can be
-# overflow, shrink or truncate
-pdf_fit_mode = "shrink"
-
-# Section level that forces a break page.
-# For example: 1 means top-level sections start in a new page
-# 0 means disabled
-pdf_break_level = 1
-
-# When a section starts in a new page, force it to be 'even', 'odd',
-# or just use 'any'
-pdf_breakside = 'any'
-
-# Insert footnotes where they are defined instead of 
-# at the end.
-#pdf_inline_footnotes = True
-
-# verbosity level. 0 1 or 2
-#pdf_verbosity = 0
-
-# If false, no index is generated.
-pdf_use_index = True
-
-# If false, no modindex is generated.
-pdf_use_modindex = False
-
-# If false, no coverpage is generated.
-#pdf_use_coverpage = True
-
-# Name of the cover page template to use
-#pdf_cover_template = 'sphinxcover.tmpl'
-
-# Documents to append as an appendix to all manuals.    
-#pdf_appendices = []
-
-# Enable experimental feature to split table cells. Use it
-# if you get "DelayedTable too big" errors
-#pdf_splittables = False
-
-# Set the default DPI for images
-#pdf_default_dpi = 72
-
-# Enable rst2pdf extension modules (default is only vectorpdf)
-# you need vectorpdf if you want to use sphinx's graphviz support
-#pdf_extensions = ['vectorpdf']
-
-# Page template name for "regular" pages
-#pdf_page_template = 'cutePage'
-
-# Show Table Of Contents at the beginning?
-pdf_use_toc = True
-
-# How many levels deep should the table of contents be?
-pdf_toc_depth = 3
-
-# Add section number to section references
-pdf_use_numbered_links = False
-
-# Background images fitting mode
-pdf_fit_background_mode = 'scale'
