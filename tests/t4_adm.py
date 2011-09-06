35eedf604b64781a6a72443dbc1a678570d59db8
Nikratio@googlemail.com <Nikratio@googlemail.com>
2011-09-06 16:11:01 -0400

    Update to new backend interface.

--- tests/t4_adm.py
+++ tests/t4_adm.py
@@ -8,15 +8,15 @@ This program can be distributed under the terms of the GNU GPLv3.
 
 from __future__ import division, print_function
 from _common import TestCase
-import unittest2 as unittest
-import tempfile
-import sys
-import os
 from cStringIO import StringIO
-import shutil
-import s3ql.cli.mkfs
-import s3ql.cli.adm
 from s3ql.backends import local
+from s3ql.backends.common import BetterBucket
+import s3ql.cli.adm
+import s3ql.cli.mkfs
+import shutil
+import sys
+import tempfile
+import unittest2 as unittest
 
 class AdmTests(TestCase):
 
@@ -24,7 +24,7 @@ class AdmTests(TestCase):
         self.cache_dir = tempfile.mkdtemp()
         self.bucket_dir = tempfile.mkdtemp()
 
-        self.bucketname = 'local://' + os.path.join(self.bucket_dir, 'mybucket')
+        self.bucketname = 'local://' + self.bucket_dir
         self.passphrase = 'oeut3d'
 
     def tearDown(self):
@@ -47,14 +47,12 @@ class AdmTests(TestCase):
         try:
             s3ql.cli.adm.main(['passphrase', self.bucketname ])
         except BaseException as exc:
+            raise
             self.fail("s3qladm failed: %s" % exc)
 
-
-        bucket = local.Connection().get_bucket(os.path.join(self.bucket_dir, 'mybucket'))
-        bucket.passphrase = passphrase_new
-
-        bucket.passphrase = bucket['s3ql_passphrase']
-        self.assertTrue(isinstance(bucket['s3ql_seq_no_0'], str))
+        plain_bucket = local.Bucket(self.bucket_dir, None, None)
+        bucket = BetterBucket(passphrase_new, 'bzip2', plain_bucket)
+        self.assertTrue(isinstance(bucket['s3ql_passphrase'], str))
 
 
 # Somehow important according to pyunit documentation
