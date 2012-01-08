c260394b48d1af06112e00ba15eb37c6b963ae50
Nikratio@googlemail.com <Nikratio@googlemail.com>
2012-01-07 22:03:17 -0500

    Redesigned benchmark output into tabular form.

--- contrib/benchmark.py
+++ contrib/benchmark.py
@@ -35,6 +35,8 @@ from s3ql.backends.local import Bucket
 from s3ql.common import setup_logging, BUFSIZE, QuietError
 from s3ql.parse_args import ArgumentParser
 
+ALGS = ('lzma', 'bzip2', 'zlib')
+
 log = logging.getLogger('benchmark')
 
 def parse_args(args):
@@ -51,6 +53,9 @@ def parse_args(args):
     parser.add_storage_url()
     parser.add_argument('file', metavar='<file>', type=argparse.FileType(mode='rb'),
                         help='File to transfer')
+    parser.add_argument('--threads', metavar='<n>', type=int, default=None,
+                        help='Also include statistics for <n> threads in results.')
+    
     parser.add_cachedir()
     return parser.parse_args(args)
 
@@ -87,7 +92,7 @@ def main(args=None):
         os.unlink('%s/bigfile' % mnt_dir)
     finally:
         subprocess.check_call(['umount.s3ql', mnt_dir])
-    log.info('Cache throughput: %.2f KB/sec', fuse_speed / 1024)
+    log.info('Cache throughput: %d KB/sec', fuse_speed / 1024)
 
     # Upload random data to prevent effects of compression
     # on the network layer
@@ -112,70 +117,76 @@ def main(args=None):
                 return (copied, stamp)
         (upload_size, upload_time) = bucket.perform_write(do_write, 's3ql_testdata')
         upload_time = time.time() - upload_time
-        log.info('Wrote %d in %.3f sec', upload_size, upload_time)
-    upload_speed = upload_size / upload_time
-    log.info('Backend throughput: %.2f KB/sec', upload_speed / 1024)
+    backend_speed = upload_size / upload_time
+    log.info('Backend throughput: %d KB/sec', backend_speed / 1024)
     bucket.delete('s3ql_testdata')
 
     src = options.file
     size = os.fstat(options.file.fileno()).st_size
     log.info('Test file size: %.2f MB', (size / 1024 ** 2))
 
-    times = dict()
-    out_sizes = dict()
-    for alg in ('lzma', 'bzip2', 'zlib'):
+    in_speed = dict()
+    out_speed = dict()
+    for alg in ALGS:
         log.info('compressing with %s...', alg)
         bucket = BetterBucket('pass', alg, Bucket('local://' + bucket_dir, None, None))
         def do_write(dst): #pylint: disable=E0102
             src.seek(0)
-            times[alg] = time.time()
+            stamp = time.time()
             while True:
                 buf = src.read(BUFSIZE)
                 if not buf:
                     break
                 dst.write(buf)
-            return dst            
-        out_sizes[alg] = bucket.perform_write(do_write, 's3ql_testdata').get_obj_size()
-        times[alg] = time.time() - times[alg]
-        log.info('%s compression speed: %.2f KB/sec (in)', alg, size / times[alg] / 1024)
-        log.info('%s compression speed: %.2f KB/sec (out)', alg,
-                 out_sizes[alg] / times[alg] / 1024)
+            return (dst, stamp)            
+        (dst_fh, stamp) = bucket.perform_write(do_write, 's3ql_testdata')
+        dt = time.time() - stamp
+        in_speed[alg] = size / dt
+        out_speed[alg] = dst_fh.get_obj_size() / dt
+        log.info('%s compression speed: %d KB/sec per thread (in)', alg, in_speed[alg] / 1024)
+        log.info('%s compression speed: %d KB/sec per thread (out)', alg, out_speed[alg] / 1024)
 
     print('')
 
-    req = dict()
-    for alg in ('lzma', 'bzip2', 'zlib'):
-        backend_req = math.ceil(upload_speed * times[alg] / out_sizes[alg])
-        fuse_req = math.ceil(fuse_speed * times[alg] / size)
-        req[alg] = min(backend_req, fuse_req)
-        print('When using %s compression, incoming writes can keep up to %d threads\n'
-              'busy. The backend can handle data from up to %d threads. Therefore,\n'
-              'the maximum achievable throughput is %.2f KB/sec with %d threads.\n'
-              % (alg, fuse_req, backend_req, min(upload_speed, fuse_speed) / 1024, req[alg]))
-
-    print('All numbers assume that the test file is representative and that',
-          'there are enough processor cores to run all threads in parallel.',
-          'To compensate for network latency, you should start about twice as',
-          'many upload threads as you need for optimal performance.\n', sep='\n')
-
+    threads = set([1,2,4,8])
     cores = os.sysconf('SC_NPROCESSORS_ONLN')
-    best_size = None
-    max_threads = cores
-    while best_size is None:
-        for alg in out_sizes:
-            if req[alg] > max_threads:
-                continue
-            if best_size is None or out_sizes[alg] < best_size:
-                best_size = out_sizes[alg]
-                best_alg = alg
-                threads = req[alg]
+    if cores != -1:
+        threads.add(cores)
+    if options.threads:
+        threads.add(options.threads)
+        
+    print('%-26s' % 'Threads:',
+          ('%12d' * len(threads)) % tuple(sorted(threads)))
+    
+    for alg in ALGS:
+        speeds = []
+        limits = []
+        for t in sorted(threads):
+            if fuse_speed > t * in_speed[alg]:
+                limit = 'CPU'
+                speed = t * in_speed[alg]
+            else:
+                limit = 'S3QL/FUSE'
+                speed = fuse_speed
 
-        max_threads = min(req.itervalues())
+            if speed / in_speed[alg] * out_speed[alg] > backend_speed:
+                limit = 'uplink'
+                speed = backend_speed * in_speed[alg] / out_speed[alg]
+                
+            limits.append(limit)
+            speeds.append(speed / 1024)    
 
-    print('This system appears to have %d cores, so best performance with maximum\n'
-          'compression ratio would be achieved by using %s compression with %d\n'
-          'upload threads.' % (cores, best_alg,
-                                2 * threads if cores >= threads else 2 * cores))
+        print('%-26s' % ('Max FS throughput (%s):' % alg),
+              ('%7d KB/s' * len(threads)) % tuple(speeds))
+        print('%-26s' % '..limited by:',
+              ('%12s' * len(threads)) % tuple(limits))
+                
+    print('')                
+    print('All numbers assume that the test file is representative and that',
+          'there are enough processor cores to run all active threads in parallel.',
+          'To compensate for network latency, you should use about twice as',
+          'many upload threads as indicated by the above table.\n', sep='\n')
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
