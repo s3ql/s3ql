3f475f3656b63f12b5b730fe682cdf48c7dc19c6
Nikolaus Rath <Nikolaus@rath.org>
2012-01-21 13:29:58 -0500

    Released 1.9

--- src/s3ql/__init__.py
+++ src/s3ql/__init__.py
@@ -14,7 +14,7 @@ __all__ = [ 'adm', 'backends', 'block_cache', 'cleanup_manager', 'common',
             'parse_args', 'remove', 'statfs', 'umount', 'VERSION',
             'CURRENT_FS_REV', 'REV_VER_MAP' ]
 
-VERSION = '1.9-rc2'
+VERSION = '1.9'
 CURRENT_FS_REV = 15
 
 # Maps file system revisions to the last S3QL version that
