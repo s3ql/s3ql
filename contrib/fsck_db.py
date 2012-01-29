366c4ad945e0f3c1e7e8f28f8ce76605af34ad0c
Nikolaus Rath <Nikolaus@rath.org>
2012-01-29 12:34:59 -0500

    Removed temporary hack for checking outdated revision.

--- contrib/fsck_db.py
+++ contrib/fsck_db.py
@@ -63,32 +63,6 @@ def main(args=None):
     options = parse_args(args)
     setup_logging(options)
 
-    # Temporary hack to allow checking previous revision with most recent fsck
-    param = pickle.load(open(options.path + '.params', 'rb'))
-    if param['revision'] == CURRENT_FS_REV - 1:
-        log.info('Upgrading...')
-        db = Connection(options.path + '.db')
-        db.execute("""
-        CREATE TABLE ext_attributes_new (
-            inode     INTEGER NOT NULL REFERENCES inodes(id),
-            name_id   INTEGER NOT NULL REFERENCES names(id),
-            value     BLOB NOT NULL,
-     
-            PRIMARY KEY (inode, name_id)               
-        )""")
-        for (inode, name, val) in db.query('SELECT inode, name, value FROM ext_attributes'):
-            db.execute('INSERT INTO ext_attributes_new (inode, name_id, value) VALUES(?,?,?)',
-                       (inode, _add_name(db, name), val))
-        db.execute('DROP TABLE ext_attributes')
-        db.execute('ALTER TABLE ext_attributes_new RENAME TO ext_attributes')
-        db.execute("""
-        CREATE VIEW ext_attributes_v AS
-        SELECT * FROM ext_attributes JOIN names ON names.id = name_id
-        """)
-        db.close()
-        param['revision'] = CURRENT_FS_REV
-        pickle.dump(param, open(options.path + '.params', 'wb'), 2)
-
     fsck = ROFsck(options.path)
     fsck.check()
 
