d544bbf814fd7e2ba48878e3464fac36233a7db9
Nikolaus Rath <Nikolaus@rath.org>
2013-05-24 22:02:47 -0700

    Whitespace cleanup.

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,40 +16,40 @@ from __future__ import division, print_function, absolute_import
 import logging.handlers
 import sys
 import os.path
-    
-log = logging.getLogger('') 
+
+log = logging.getLogger('')
 
 def pytest_addoption(parser):
     group = parser.getgroup("terminal reporting")
     group._addoption("--logdebug", action="append", metavar='<module>',
                      help="Activate debugging output from <module> for tests. Use `all` "
-                        "to get debug messages from all modules. This option can be "
-                        "specified multiple times.")    
+                          "to get debug messages from all modules. This option can be "
+                          "specified multiple times.")
 
     group = parser.getgroup("general")
     group._addoption("--installed", action="store_true", default=False,
-                     help="Test the installed package.") 
-        
+                     help="Test the installed package.")
+    
 def pytest_configure(config):
 
     logdebug = config.getoption('logdebug')
-    
-    # If we are running from the S3QL source directory, make sure that we 
+
+    # If we are running from the S3QL source directory, make sure that we
     # load modules from here
     basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    if not config.getoption('installed'): 
+    if not config.getoption('installed'):
         if (os.path.exists(os.path.join(basedir, 'setup.py')) and
             os.path.exists(os.path.join(basedir, 'src', 's3ql', '__init__.py'))):
             sys.path = [os.path.join(basedir, 'src')] + sys.path
-        
-    # When running from HG repo, enable all warnings    
+
+    # When running from HG repo, enable all warnings
     if os.path.exists(os.path.join(basedir, 'MANIFEST.in')):
         import warnings
         warnings.resetwarnings()
         warnings.simplefilter('error')
-        
+
         # Resource warnings can be emitted from __del__, so turning them into
-        # exceptions doesn't help (they'll get printed to stdout and captured 
+        # exceptions doesn't help (they'll get printed to stdout and captured
         # by py.test)
         warnings.simplefilter('default', ResourceWarning)
 
@@ -66,7 +66,7 @@ def pytest_configure(config):
                                       '[%(name)s] %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
         handler.setFormatter(formatter)
         root_logger.addHandler(handler)
-        
+
         if logdebug is not None:
             root_logger.setLevel(logging.DEBUG)
             if 'all' not in logdebug:
