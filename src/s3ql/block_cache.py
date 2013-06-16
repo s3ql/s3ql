cf5055379985103a097ea63a353a429954cf0213
Nikolaus Rath <Nikolaus@rath.org>
2013-06-15 19:43:38 -0700

    Only create cache path if it doesn't exist.
    --HG--
    branch : maint-1.x
    extra : source : d2c3f6b0032dedd972f8c5e9c5dd2a848e16a965

--- src/s3ql/block_cache.py
+++ src/s3ql/block_cache.py
@@ -227,7 +227,8 @@ class BlockCache(object):
         self.removal_threads = []
         self.transfer_completed = SimpleEvent()
 
-        os.mkdir(self.path)
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
 
     def __len__(self):
         '''Get number of objects in cache'''
