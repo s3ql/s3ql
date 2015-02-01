77a11e4f7d69b23409e3053ee64c3285b7fab7d6
Nikolaus Rath <Nikolaus@rath.org>
2015-01-31 18:29:46 -0800

    Added code to convert all storage objects to newest revision.
    --HG--
    extra : rebase_source : f5bd2c9d9764ce56beb334713cd1323ec8f9177d
    extra : histedit_source : 8bbc651fe44c4ca4833ae0edc2e98aa766e9d912

--- src/s3ql/adm.py
+++ src/s3ql/adm.py
@@ -7,17 +7,19 @@ This program can be distributed under the terms of the GNU GPLv3.
 '''
 
 from .logging import logging, QuietError, setup_logging
-from . import CURRENT_FS_REV, REV_VER_MAP, PICKLE_PROTOCOL
+from . import CURRENT_FS_REV, REV_VER_MAP, PICKLE_PROTOCOL, BUFSIZE
 from .backends.comprenc import ComprencBackend
 from .database import Connection
 from .common import (get_backend_cachedir, get_seq_no, stream_write_bz2,
-                     stream_read_bz2, is_mounted, get_backend,
-                     pretty_print_size, split_by_n)
+                     stream_read_bz2, is_mounted, AsyncFn, get_backend,
+                     get_backend_factory, pretty_print_size, split_by_n,
+                     handle_on_return)
 from .metadata import restore_metadata, cycle_metadata, dump_metadata
 from .parse_args import ArgumentParser
 from datetime import datetime as Datetime
 from getpass import getpass
 from base64 import b64encode
+from queue import Queue, Full as QueueFull
 import os
 import pickle
 import shutil
@@ -25,7 +27,6 @@ import sys
 import tempfile
 import textwrap
 import time
-import atexit
 
 log = logging.getLogger(__name__)
 
@@ -47,14 +48,16 @@ def parse_args(args):
                                        help='may be either of')
     subparsers.add_parser("passphrase", help="change file system passphrase",
                           parents=[pparser])
-    subparsers.add_parser("upgrade", help="upgrade file system to newest revision",
-                          parents=[pparser])
     subparsers.add_parser("clear", help="delete file system and all data",
                           parents=[pparser])
     subparsers.add_parser("download-metadata",
                           help="Interactively download metadata backups. "
                                "Use only if you know what you are doing.",
                           parents=[pparser])
+    sparser = subparsers.add_parser("upgrade", help="upgrade file system to newest revision",
+                          parents=[pparser])
+    sparser.add_argument("--threads", type=int, default=20,
+                        help='Number of threads to use')
 
     parser.add_debug()
     parser.add_quiet()
@@ -84,24 +87,18 @@ def main(args=None):
         raise QuietError('Can not work on mounted file system.')
 
     if options.action == 'clear':
-        backend = get_backend(options, plain=True)
-        atexit.register(backend.close)
-        return clear(backend,
-                     get_backend_cachedir(options.storage_url, options.cachedir))
-
-    backend = get_backend(options)
-    atexit.register(backend.close)
+        with get_backend(options, plain=True) as backend:
+            return clear(backend, options)
 
     if options.action == 'upgrade':
-        return upgrade(backend, get_backend_cachedir(options.storage_url,
-                                                   options.cachedir))
+        return upgrade(options)
+    
+    with get_backend(options) as backend:
+        if options.action == 'passphrase':
+            return change_passphrase(backend)
 
-    if options.action == 'passphrase':
-        return change_passphrase(backend)
-
-    if options.action == 'download-metadata':
-        return download_metadata(backend, options.storage_url)
-
+        if options.action == 'download-metadata':
+            return download_metadata(backend, options.storage_url)
 
 def download_metadata(backend, storage_url):
     '''Download old metadata backups'''
@@ -185,8 +182,7 @@ def change_passphrase(backend):
     backend['s3ql_passphrase_bak3'] = data_pw
     backend.passphrase = data_pw
 
-
-def clear(backend, cachepath):
+def clear(backend, options):
     print('I am about to delete all data in %s.' % backend,
           'This includes any S3QL file systems as well as any other stored objects.',
           'Please enter "yes" to continue.', '> ', sep='\n', end='')
@@ -196,7 +192,7 @@ def clear(backend, cachepath):
         raise QuietError()
 
     log.info('Deleting...')
-
+    cachepath = get_backend_cachedir(options.storage_url, options.cachedir)
     for suffix in ('.db', '.params'):
         name = cachepath + suffix
         if os.path.exists(name):
@@ -225,10 +221,16 @@ def get_old_rev_msg(rev, prog):
         ''' % { 'version': REV_VER_MAP[rev],
                 'prog': prog })
 
-def upgrade(backend, cachepath):
+
+@handle_on_return
+def upgrade(options, on_return):
     '''Upgrade file system to newest revision'''
 
     log.info('Getting file system parameters..')
+    cachepath = get_backend_cachedir(options.storage_url, options.cachedir)
+
+    backend_factory = get_backend_factory(options)
+    backend = on_return.enter_context(backend_factory())
 
     # Check for cached metadata
     db = None
@@ -332,8 +334,9 @@ def upgrade(backend, cachepath):
                 backend[obj_id] = data_pw
         backend.passphrase = data_pw
 
-    # We have also added a new metadata format, but old objects can still
-    # be read so we don't need to do any other conversion work.
+    # Upgrade all objects, so that we can remove legacy conversion
+    # routines in the next release.
+    update_obj_metadata(backend, backend_factory, db, options.threads)
 
     log.info('Dumping metadata...')
     with tempfile.TemporaryFile() as fh:
@@ -372,28 +375,145 @@ def upgrade(backend, cachepath):
               '---END MASTER KEY---',
               sep='\n')
 
-# This should be used on the *next* fs revision update to ensure that all
-# objects conform to newest standards, so we can drop the legacy routines from
-# backend/common.py as well as the legacy metadata handling in backends/s3c.py
-def update_obj_metadata(backend, db):
+@handle_on_return
+def update_obj_metadata(backend, backend_factory, db,
+                        thread_count, on_return):
     '''Upgrade metadata of storage objects'''
 
-    # TODO: Don't forget the s3ql_passphrase object and backups
-    # (we need to be able to load the metadata when renaming them)
-
     plain_backend = backend.backend
-    for (obj_id,) in db.query('SELECT id FROM obj_ids'):
-        meta = plain_backend.lookup('s3ql_data_%d' % obj_id)
-        if 'format_version' in meta:
+
+    # No need to update sequence number, since we are going to
+    # write out a new one after the upgrade.
+    if backend.passphrase is None:
+        extra_objects = { 's3ql_metadata' }
+    else:
+        extra_objects = { 's3ql_metadata',
+                          's3ql_passphrase', 's3ql_passphrase_bak1',
+                          's3ql_passphrase_bak2', 's3ql_passphrase_bak3' }
+
+    for i in range(30):
+        obj_id = 's3ql_metadata_bak_%d' % i
+        if obj_id in plain_backend:
+            extra_objects.add(obj_id)
+
+    def yield_objects():
+        for (id_,) in db.query('SELECT id FROM objects'):
+            yield 's3ql_data_%d' % id_
+        for obj_id in extra_objects:
+            yield obj_id
+    total = db.get_val('SELECT COUNT(id) FROM objects') + len(extra_objects)
+
+    queue = Queue(maxsize=thread_count)
+    threads = []
+    for _ in range(thread_count):
+        t = AsyncFn(upgrade_loop, queue, on_return.push(backend_factory()))
+        # Don't wait for worker threads, gives deadlock if main thread
+        # terminates with exception
+        t.daemon = True
+        t.start()
+        threads.append(t)
+
+    # Updating this value is prone to race conditions. However,
+    # we don't care because this is for an approximate progress
+    # output only.
+    queue.rewrote_size = 0
+    stamp = 0
+    for (i, obj_id) in enumerate(yield_objects()):
+        stamp2 = time.time()
+        if stamp2 - stamp > 1:
+            sys.stdout.write('\r..processed %d/%d objects (%.1f%%, %s rewritten)..'
+                             % (i, total, i/total*100,
+                                pretty_print_size(queue.rewrote_size)))
+            sys.stdout.flush()
+            stamp = stamp2
+
+            # Terminate early if any thread failed with an exception
+            for t in threads:
+                if not t.is_alive():
+                    t.join_and_raise()
+
+        # Avoid blocking if all threads terminated
+        while True:
+            try:
+                queue.put(obj_id, timeout=1)
+            except QueueFull:
+                pass
+            else:
+                break
+            for t in threads:
+                if not t.is_alive():
+                    t.join_and_raise()
+
+    queue.maxsize += len(threads)
+    for t in threads:
+        queue.put(None)
+
+    for t in threads:
+        t.join_and_raise()
+
+    sys.stdout.write('\n')
+
+def upgrade_loop(queue, backend):
+    plain_backend = backend.backend
+
+    while True:
+        obj_id = queue.get()
+        if obj_id is None:
+            break
+
+        meta = plain_backend.lookup(obj_id)
+        if meta.get('format_version', 0) == 2:
             continue
+
+        # For important objects, we make a copy first (just to be safe)
+        if not obj_id.startswith('s3ql_data'):
+            plain_backend.copy(obj_id, 's3ql_pre2.13' + obj_id[4:])
+
+        # When reading passphrase objects, we have to use the
+        # "outer" password
+        if obj_id.startswith('s3ql_passphrase'):
+            data_pw = backend.passphrase
+            backend.passphrase = backend.fs_passphrase
+
         meta = backend._convert_legacy_metadata(meta)
-
         if meta['encryption'] == 'AES':
-            #rewrite_legacy_object(obj_id, meta)
-            raise RuntimeError("Don't know how to convert legacy objects yet")
+            # Two statements to reduce likelihood of update races
+            size = rewrite_legacy_object(backend, obj_id)
+            queue.rewrote_size += size
         else:
-            plain_backend.update_meta('s3ql_data_%d' % obj_id, meta)
+            plain_backend.update_meta(obj_id, meta)
 
+        if obj_id.startswith('s3ql_passphrase'):
+            backend.passphrase = data_pw
+
+def rewrite_legacy_object(backend, obj_id):
+    with tempfile.TemporaryFile() as tmpfh:
+
+        # Read object
+        def do_read(fh):
+            tmpfh.seek(0)
+            tmpfh.truncate()
+            while True:
+                buf = fh.read(BUFSIZE)
+                if not buf:
+                    break
+                tmpfh.write(buf)
+            return fh.metadata
+
+        meta = backend.perform_read(do_read, obj_id)
+
+        # Write object
+        def do_write(fh):
+            tmpfh.seek(0)
+            while True:
+                buf = tmpfh.read(BUFSIZE)
+                if not buf:
+                    break
+                fh.write(buf)
+            return fh
+        out_fh = backend.perform_write(do_write, obj_id, meta)
+
+        return out_fh.get_obj_size()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
