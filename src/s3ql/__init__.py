4587dc3b0050f89c261fbc35273e517e9bfe8da3
Nikolaus Rath <Nikolaus@rath.org>
2017-06-23 09:38:45 -0700

    Released 2.22

--- src/s3ql/__init__.py
+++ src/s3ql/__init__.py
@@ -22,7 +22,7 @@ __all__ = [ 'adm', 'backends', 'block_cache', 'common', 'calc_mro',
             'REV_VER_MAP', 'RELEASE', 'BUFSIZE',
             'CTRL_NAME', 'CTRL_INODE' ]
 
-VERSION = '2.21'
+VERSION = '2.22'
 RELEASE = '%s' % VERSION
 
 # TODO: On next revision bump, consider removing support for TIME
