9499338cfe130a2da6ce3785da7ac09edd0ff9f9
Erick Brown <erick.brown@logmein.com>
2019-09-06 17:52:30 -0500

    Updated to allow for separate user and project domains

--- src/s3ql/backends/swift.py
+++ src/s3ql/backends/swift.py
@@ -40,7 +40,7 @@ class Backend(AbstractBackend, metaclass=ABCDocstMeta):
     hdr_prefix = 'X-Object-'
     known_options = {'no-ssl', 'ssl-ca-path', 'tcp-timeout',
                      'disable-expect100', 'no-feature-detection',
-                     'domain'}
+                     'domain', 'project_domain'}
 
     _add_meta_headers = s3c.Backend._add_meta_headers
     _extractmeta = s3c.Backend._extractmeta
