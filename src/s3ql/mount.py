s3ql/s3ql

commit b04bb970962b0b7d8f284bd5ed1dafd56d9a09b0

Migrate from trio < 0.12 to trio 0.12.
Fixes: #134.

src/s3ql/mount.py
@@ -602,7 +602,6 @@ class MetadataUploadTask:
         while not self.quit:
             with trio.move_on_after(self.interval):
                 await self.event.wait()
-            self.event.clear()
 
             if self.quit:
                 break
@@ -631,7 +630,7 @@ class MetadataUploadTask:
 
                 # Temporarily decrease sequence no, this is not the final upload
                 self.param['seq_no'] -= 1
-                await trio.run_sync_in_worker_thread(
+                await trio.to_thread.run_sync(
                     upload_metadata, backend, fh, self.param)
                 self.param['seq_no'] += 1
 

setup.py
@@ -143,9 +143,8 @@ def main():
                      'google-auth',
                      'google-auth-oauthlib',
 
-                     # earlier trio versions swallow exceptions raised by
-                     # pyfuse3.main().
-                     'trio >= 0.9',
+                     # Need trio.to_thread/from_thread
+                     'trio >= 0.12',
                      'pyfuse3 >= 1.0, < 2.0' ]
     if sys.version_info < (3, 7, 0):
         required_pkgs.append('async_generator')

src/s3ql/block_cache.py
@@ -215,7 +215,7 @@ class BlockCache(object):
     def init(self, threads=1):
         '''Start worker threads'''
 
-        self.portal = trio.BlockingTrioPortal()
+        self.trio_token = trio.hazmat.current_trio_token()
         self.to_upload = trio.open_memory_channel(0)
         for _ in range(threads):
             t = threading.Thread(target=self._upload_loop)
@@ -271,7 +271,7 @@ class BlockCache(object):
 
         log.debug('waiting for upload threads...')
         for t in self.upload_threads:
-            await trio.run_sync_in_worker_thread(t.join)
+            await trio.to_thread.run_sync(t.join)
 
         log.debug('waiting for removal threads...')
         for t in self.removal_threads:
@@ -306,7 +306,8 @@ class BlockCache(object):
 
         while True:
             log.debug('reading from upload queue...')
-            tmp = self.portal.run(self.to_upload[1].receive)
+            tmp = trio.from_thread.run(
+                self.to_upload[1].receive, trio_token=self.trio_token)
 
             if tmp is QuitSentinel:
                 log.debug('got QuitSentinel')
@@ -382,7 +383,7 @@ class BlockCache(object):
             success = True
         finally:
             self.in_transit.remove(el)
-            self.portal.run(with_event_loop)
+            trio.from_thread.run(with_event_loop, trio_token=self.trio_token)
 
 
     async def wait(self):
@@ -445,7 +446,7 @@ class BlockCache(object):
                         break
                     sha.update(buf)
                 return sha.digest()
-            hash_ = await trio.run_sync_in_worker_thread(with_lock_released)
+            hash_ = await trio.to_thread.run_sync(with_lock_released)
 
         except:
             if added_to_transit:
@@ -591,7 +592,7 @@ class BlockCache(object):
         try:
             self.to_remove.put(obj_id, block=False)
         except QueueFull:
-            await trio.run_sync_in_worker_thread(self._queue_removal, obj_id)
+            await trio.to_thread.run_sync(self._queue_removal, obj_id)
 
     def transfer_in_progress(self):
         '''Return True if there are any cache entries being uploaded'''
@@ -718,7 +719,7 @@ class BlockCache(object):
                 def with_lock_released():
                     with self.backend_pool() as backend:
                         backend.perform_read(do_read, 's3ql_data_%d' % obj_id)
-                await trio.run_sync_in_worker_thread(with_lock_released)
+                await trio.to_thread.run_sync(with_lock_released)
 
                 tmpfh.flush()
                 os.fsync(tmpfh.fileno())

tests/t2_block_cache.py
@@ -100,14 +100,14 @@ async def ctx():
 
         cache = BlockCache(ctx.backend_pool, ctx.db, ctx.cachedir + "/cache",
                            ctx.max_obj_size * 100)
-        cache.portal = trio.BlockingTrioPortal()
+        cache.trio_token = trio.hazmat.current_trio_token()
         ctx.cache = cache
 
         # Monkeypatch around the need for removal and upload threads
         cache.to_remove = DummyQueue(cache)
         class DummyChannel:
             async def send(self, arg):
-                await trio.run_sync_in_worker_thread(cache._do_upload, *arg)
+                await trio.to_thread.run_sync(cache._do_upload, *arg)
         cache.to_upload = (DummyChannel(), None)
 
         try:

tests/t3_fs_api.py
@@ -86,7 +86,7 @@ async def ctx():
 
     cache = BlockCache(ctx.backend_pool, ctx.db, ctx.cachedir + "/cache",
                        ctx.max_obj_size * 5)
-    cache.portal = trio.BlockingTrioPortal()
+    cache.trio_token = trio.hazmat.current_trio_token()
     ctx.cache = cache
     ctx.server = fs.Operations(cache, ctx.db, ctx.max_obj_size,
                                 InodeCache(ctx.db, 0))
@@ -96,7 +96,7 @@ async def ctx():
     cache.to_remove = DummyQueue(cache)
     class DummyChannel:
         async def send(self, arg):
-            await trio.run_sync_in_worker_thread(cache._do_upload, *arg)
+            await trio.to_thread.run_sync(cache._do_upload, *arg)
     cache.to_upload = (DummyChannel(), None)
 
     # Keep track of unused filenames

commit 65ee19a3bfa4dd257c05ab94a639b4f1d4576603

Fix metadata upload
Because we did not reset the event object, we are currently updating continously after the
first upload.

(This was broken in commit b04bb970962b0b).

Fixes: #179.

src/s3ql/mount.py
@@ -605,7 +605,7 @@ class MetadataUploadTask:
             else:
                 with trio.move_on_after(self.interval):
                     await self.event.wait()
-
+            self.event = trio.Event()  # reset
             if self.quit:
                 break
 