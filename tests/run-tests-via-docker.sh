32e4100735637ad9aaade910c1d40cb8cf73a0f5
Daniel Jagszent <daniel@jagszent.de>
2020-09-30 14:21:47 +0200

    Runs tests that need FUSE with `tests/run-tests-via-docker.sh`
    Since `docker build` cannot use the device `/dev/fuse` we need to split the test running up into two parts, building the container and then run the tests.
    
    Added the possibility to pass in pytest command line arguments.
    E.g. `tests/run-tests-via-docker.sh tests/t5_failsafe.py::TestSigInt::test` will only execute this one test.

--- tests/run-tests-via-docker.sh
+++ tests/run-tests-via-docker.sh
@@ -4,6 +4,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR/.." || exit 1
 
-# run tests in docker and copy documentation to /build/
-# use BuildKit when available so that the --output option works on macOS
-DOCKER_BUILDKIT=1 docker build -f ./tests/Dockerfile -o "./build" .
+set -e
+
+# build a fresh container (with current sources, will mostly get cached in subsequent calls)
+docker build -f ./tests/Dockerfile  -t s3ql-tests .
+# run tests in docker, pass down arguments to pytest
+if [ "$#" -eq 0 ]; then
+  PY_TEST_ARGS=( tests )
+else
+  PY_TEST_ARGS=( "$@" )
+fi
+docker run --rm \
+           --device /dev/fuse \
+           --cap-add SYS_ADMIN \
+           s3ql-tests python -m pytest "${PY_TEST_ARGS[@]}"
