d80385bf85bcdca93f2d3c2e887d041ceb6e979b
nand <ndeschildre@gmail.com>
2021-01-28 14:54:39 +0100

    test_cp_inode_invalidate test: Handle last comment.

--- tests/t5_cp_mount_helper.py
+++ tests/t5_cp_mount_helper.py
@@ -9,14 +9,13 @@ sys.path = [os.path.join(basedir, 'src')] + sys.path
 # Override pyfuse3.invalidate_inode : Drop kernel dentries and inodes cache
 # just before calling pyfuse3.invalidate_inode
 import pyfuse3
-from _pytest.monkeypatch import MonkeyPatch
 pyfuse3_invalidate_inode = pyfuse3.invalidate_inode
 def patched_pyfuse3_invalidate_inode(inode):
     # echo 2 > /proc/sys/vm/drop_caches : Drop kernel dentries and inodes cache
     with open("/proc/sys/vm/drop_caches", "w") as drop_caches:
         drop_caches.write("2\n")
     pyfuse3_invalidate_inode(inode)
-MonkeyPatch().setattr('pyfuse3.invalidate_inode', patched_pyfuse3_invalidate_inode)
+pyfuse3.invalidate_inode = patched_pyfuse3_invalidate_inode
 
 import s3ql.mount
 s3ql.mount.main(sys.argv[1:])
