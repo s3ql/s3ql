4f4e6e2c9aef84e73bb771153edb9d3440b83e93
Nikolaus Rath <Nikolaus@rath.org>
2021-11-07 11:15:54 +0000

    Released 3.8.0

--- make_release.sh
+++ make_release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-python3 setup.py build_cython build_ext --inplace`
+python3 setup.py build_cython build_ext --inplace
 ./build_docs.sh
 (cd doc/pdf && latexmk)
 python3 ./setup.py sdist
--- src/s3ql/__init__.py
+++ src/s3ql/__init__.py
@@ -38,7 +38,7 @@ assert logging.LOG_ONCE  # prevent warnings about unused module
 
 from pyfuse3 import ROOT_INODE
 
-VERSION = '3.7.3'
+VERSION = '3.8.0'
 RELEASE = '%s' % VERSION
 
 # TODO: On next revision bump, remove upgrade code from backend/comprenc.py and
