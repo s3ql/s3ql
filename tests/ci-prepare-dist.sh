fede220acbbc86567b8e350eeeb2dea814ee7395
Daniel Jagszent <daniel@jagszent.de>
2024-05-18 00:42:56 +0200

    chore: also run tests on Ubuntu 24.04

--- tests/ci-prepare-dist.sh
+++ tests/ci-prepare-dist.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+PIP="sudo python3 -m pip"
+if [ -f venv/bin/activate ]; then
+  . venv/bin/activate
+  PIP="pip"
+fi
+
 os="$(lsb_release --short --id)-$(lsb_release --short --release)"
 
 sudo apt install -y \
@@ -24,18 +30,21 @@ sudo apt install -y \
 # The python3-attr that is shipped with Ubuntu 20.04 is incompatible
 # with python3-trio. *sigh*
 if [ "${os}" = "Ubuntu-20.04" ]; then
-    sudo python3 -m pip install "attrs >= 20.1.0, < 21.0.0 "
+    $PIP install "attrs >= 20.1.0, < 21.0.0 "
 else
     sudo apt install -y python3-attr
 fi
 
 
 # Where packages are not available, download from pypi instead (but pin to specific versions).
-sudo python3 -m pip install \
-     "pytest_trio == 0.6.0"
+if [ "${os}" = "Ubuntu-24.04" ]; then
+  sudo apt install -y python3-pytest-trio
+else
+  $PIP install "pytest_trio == 0.6.0"
+fi
 
 if [ "${os}" = "Ubuntu-20.04" ]; then
-    sudo python3 -m pip install \
+    $PIP install \
          "pyfuse3 >= 3.2.0, < 4.0" \
          "google-auth-oauthlib >= 0.4.0, < 0.5.0"
 else
@@ -47,4 +56,4 @@ fi
 echo "Current libsqlite3-dev version: $(dpkg-query --show --showformat='${Version}' libsqlite3-dev)"
 
 echo "Installed PIP versions:"
-python3 -m pip freeze
+$PIP freeze
