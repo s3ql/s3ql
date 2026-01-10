a1015263bce1e9f8e7d2d9f1c5c26a3353269cdb
Nikolaus Rath <Nikolaus@rath.org>
2026-01-10 12:25:21 +0000

    Remove obsolete files.

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-;; This tells Emacs to automatically run whitespace-cleanup when
-;; editing any file in the S3QL source tree.
-((nil . ((eval . (add-hook 'before-save-hook 'whitespace-cleanup nil t)))))
